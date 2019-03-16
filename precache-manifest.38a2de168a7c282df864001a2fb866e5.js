32fc588199ebc9ed61fbba65fc7da39595cad6c7
YjjTT <yjjtt@tempot.com>
2019-03-16 12:01:45 +0800

    Updates

---
+++ precache-manifest.38a2de168a7c282df864001a2fb866e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db8dd80c4c7a7f8447c9",
+    "url": "/antd-demo/static/css/main.c2796150.chunk.css"
+  },
+  {
+    "revision": "db8dd80c4c7a7f8447c9",
+    "url": "/antd-demo/static/js/main.1f0edfed.chunk.js"
+  },
+  {
+    "revision": "69ac5fd5424f414ef63d",
+    "url": "/antd-demo/static/js/runtime~main.17a3d04d.js"
+  },
+  {
+    "revision": "5836932d5c1065c0bb17",
+    "url": "/antd-demo/static/css/2.f9ff76ec.chunk.css"
+  },
+  {
+    "revision": "5836932d5c1065c0bb17",
+    "url": "/antd-demo/static/js/2.1a111779.chunk.js"
+  },
+  {
+    "revision": "f16f5a400111c456048eebf4b95edc45",
+    "url": "/antd-demo/index.html"
+  }
+];
